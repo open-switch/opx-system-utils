0985cdfcc84d8032bebe64d5957831f86871a781
Rakesh Datta <rakesh.datta@dell.com>
2018-11-08 12:15:14 -0800

    updated the string format to correctly map colors for chart
    Signed-off-by: Rakesh Datta <rakesh.datta@dell.com>

--- scripts/lib/python/plugins/analytics.py
+++ scripts/lib/python/plugins/analytics.py
@@ -153,7 +153,7 @@ def get_spaced_colors(n):
     interval = int(max_value / n)
     colors = [hex(I)[2:].zfill(6) for I in range(0, max_value, interval)]
     
-    return [(int(i[:2], 16), int(i[2:4], 16), int(i[4:], 16)) for i in colors]
+    return ["rgb%s" % (str((int(i[:2], 16), int(i[2:4], 16), int(i[4:], 16)))) for i in colors]
 #******************************************************************************
 
 num_hours_collect=3
